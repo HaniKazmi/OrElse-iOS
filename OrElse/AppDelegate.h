59762bb385846a57a8bd0e6ad752580cccc8ad93
Hani Kazmi <hanikazmi@hanis-mbp.home>
2014-08-05 20:37:39 +0100

    Initial Commit

---
+++ OrElse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrElse
+//
+//  Created by Hani Kazmi on 05/08/2014.
+//  Copyright (c) 2014 Hani Kazmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrElse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrElse
+//
+//  Created by Hani Kazmi on 05/08/2014.
+//  Copyright (c) 2014 Hani Kazmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
