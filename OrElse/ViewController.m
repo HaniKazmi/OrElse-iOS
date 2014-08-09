6b631ac26a9492f5c71b20f75c6c2a341f1ce257
Hani Kazmi <hanikazmi@hanis-mbp.home>
2014-08-09 19:10:53 +0100

    Code cleanup

--- OrElse/ViewController.m
+++ OrElse/ViewController.m
@@ -17,10 +17,6 @@
             
 - (void)viewDidLoad {
     [super viewDidLoad];
-    FBLoginView *loginView = [[FBLoginView alloc] init];
-    loginView.readPermissions = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location", @"read_friendlists", @"friends_hometown", @"friends_birthday",@"friends_location", @"user_friends", @"publish_actions"];
-    [self.view addSubview:loginView];
-   // [self loginButtonTouchHandler:nil];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -43,7 +39,7 @@
 
 - (IBAction)loginButtonTouchHandler:(id)sender  {
     // The permissions requested from the user
-    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+    NSArray *permissionsArray = @[ @"user_about_me", @"user_friends", @"read_friendlists"];
     
     // Login PFUser using Facebook
     [PFFacebookUtils initializeFacebook];
@@ -62,61 +58,25 @@
 
 - (IBAction)sendMessageButtonHandler:(id)sender  {
     // The permissions requested from the user
-    FBRequest* friendsRequest = [FBRequest requestForGraphPath:@"/me/taggable_friends"];
-    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
-                                                  NSDictionary* result,
-                                                  NSError *error) {
-         
-     //   NSLog([result description]);
+    FBLinkShareParams *params = [[FBLinkShareParams alloc] init];
+
+    params.link =
+    [NSURL URLWithString:@"http://orelseapp.github.io"];
+    params.name = @"OrElse";
+    params.caption = @"Hi Ammaar.";
+    params.picture = [NSURL URLWithString:@"http://i.imgur.com/g3Qc1HN.png"];
+    params.linkDescription = @"Did this work?";
+    
+    [FBDialogs presentMessageDialogWithParams:params clientState:nil handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+        if(error) {
+            // An error occurred, we need to handle the error
+            // See: https://developers.facebook.com/docs/ios/errors
+            NSLog([NSString stringWithFormat:@"Error messaging link: %@", error.description]);
+        } else {
+            // Success
+            NSLog(@"result %@", results);
+        }
     }];
-    
-//    FBLinkShareParams *params = [[FBLinkShareParams alloc] init];
-    
-    [FBWebDialogs
-     presentRequestsDialogModallyWithSession:[FBSession activeSession]
-     message:@"Work"
-     title:@"Hello"
-     parameters:nil
-     handler:^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
-         if (error) {
-             // Error launching the dialog or sending the request.
-             NSLog(@"Error sending request.");
-         } else {
-             if (result == FBWebDialogResultDialogNotCompleted) {
-                 // User clicked the "x" icon
-                 NSLog(@"User canceled request.");
-             } else {
-                 // Handle the send request callback
-                 NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
-                 if (![urlParams valueForKey:@"request"]) {
-                     // User clicked the Cancel button
-                     NSLog(@"User canceled request.");
-                 } else {
-                     // User clicked the Send button
-                     NSString *requestID = [urlParams valueForKey:@"request"];
-                     NSLog(@"Request ID: %@", requestID);
-                 }
-             }
-         }
-     }];
-//    params.link =
-//    [NSURL URLWithString:@"https://developers.facebook.com/docs/ios/share/"];
-//    params.name = @"OrElse";
-//    params.caption = @"Hi Ammaar.";
-//    params.picture = [NSURL URLWithString:@"http://i.imgur.com/g3Qc1HN.png"];
-//    params.linkDescription = @"Did this work?";
-//    
-//    [FBDialogs presentMessageDialogWithLink:[NSURL URLWithString:@"https://developers.facebook.com/ios"]
-//                                    handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
-//                                        if(error) {
-//                                            // An error occurred, we need to handle the error
-//                                            // See: https://developers.facebook.com/docs/ios/errors
-//                                            NSLog([NSString stringWithFormat:@"Error messaging link: %@", error.description]);
-//                                        } else {
-//                                            // Success
-//                                            NSLog(@"result %@", results);
-//                                        }
-//                                    }];
 
 }
 
