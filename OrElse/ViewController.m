d0c3d725c37d63e9ad06d56be75a986a16e7a5fe
Hani Kazmi <hanikazmi@hanis-mbp.home>
2014-08-09 21:26:08 +0100

    Tasks can now be saved and retrieved

--- OrElse/ViewController.m
+++ OrElse/ViewController.m
@@ -8,8 +8,13 @@
 
 #import "ViewController.h"
 #import <Parse/Parse.h>
+#import "AppDelegate.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *taskTextField;
+@property (weak, nonatomic) IBOutlet UITextField *DateTextField;
+
 
 @end
 
@@ -41,10 +46,8 @@
     // The permissions requested from the user
     NSArray *permissionsArray = @[ @"user_about_me", @"user_friends", @"read_friendlists"];
     
-    // Login PFUser using Facebook
     [PFFacebookUtils initializeFacebook];
     [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
-       // [_activityIndicator stopAnimating]; // Hide loading indicator
         
         if (!user) {
             if (!error) {
@@ -58,26 +61,58 @@
 
 - (IBAction)sendMessageButtonHandler:(id)sender  {
     // The permissions requested from the user
-    FBLinkShareParams *params = [[FBLinkShareParams alloc] init];
-
-    params.link =
-    [NSURL URLWithString:@"http://orelseapp.github.io"];
-    params.name = @"OrElse";
-    params.caption = @"Hi Ammaar.";
-    params.picture = [NSURL URLWithString:@"http://i.imgur.com/g3Qc1HN.png"];
-    params.linkDescription = @"Did this work?";
+    PFUser *currentUser = [PFUser currentUser];
+    PFObject *task = [PFObject objectWithClassName:@"Task"];
+    task[@"task"] = self.taskTextField.text;
+    task[@"date"] = self.DateTextField.text;
+    task[@"creatorID"] = currentUser.username;
+  
     
-    [FBDialogs presentMessageDialogWithParams:params clientState:nil handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+    [FBWebDialogs
+     presentRequestsDialogModallyWithSession:[FBSession activeSession]
+     message:@"Frape Me"
+     title:@"OrElse"
+     parameters:nil
+     handler:^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+         if (error) {
+             // Error launching the dialog or sending the request.
+             NSLog(@"Error sending request.");
+         } else {
+             if (result == FBWebDialogResultDialogNotCompleted) {
+                 // User clicked the "x" icon
+                 NSLog(@"User canceled request.");
+             } else {
+                 // Handle the send request callback
+                 NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
+                 if (![urlParams valueForKey:@"request"]) {
+                     // User clicked the Cancel button
+                     NSLog(@"User canceled request.");
+                 } else {
+                     // User clicked the Send button
+                     NSString *requestID = [urlParams valueForKey:@"request"];
+                     NSLog(@"Request ID: %@", requestID);
+                    task[@"supervisorID"] = urlParams[@"to%5B0%5D"];
+                    [task saveInBackground];
+                 }
+             }
+         }
+     }];
+
+}
+- (IBAction)showMyTaks {
+    [FBRequestConnection startForMeWithCompletionHandler:^(FBRequestConnection *connection,
+                                                           NSDictionary<FBGraphUser> *me,
+                                                           NSError *error) {
         if(error) {
-            // An error occurred, we need to handle the error
-            // See: https://developers.facebook.com/docs/ios/errors
-            NSLog([NSString stringWithFormat:@"Error messaging link: %@", error.description]);
-        } else {
-            // Success
-            NSLog(@"result %@", results);
+
+            return;
         }
+        PFQuery *query = [PFQuery queryWithClassName:@"Task"];
+        [query whereKey:@"supervisorID" equalTo:me.objectID];
+        NSLog(@"My Task are %@", [query findObjects]);
     }];
 
+    
 }
 
 @end
